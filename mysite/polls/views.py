f03ff4c522d67eea30cac80de5c69a76cf4c3e1c
joaco <joaco.munoz04@gmail.com>
2024-04-13 00:30:02 -0300

    [Parte 3.1] Añade vistas de detalle, resultados y votaciones

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,5 +3,16 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Estás en el polls index")
+
+def detail(request, question_id):
+    return HttpResponse("Estás viendo una pregunta %s." % question_id)
+
+
+def results(request, question_id):
+    response = "Estás viendo los resultados de una pregunta %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("Estás votando")
     
 # Create your views here.
